7a6e0c89380b751dd18787ef3ec5293e234f3f87
Bogdan <bodyaghnidenko@gmail.com>
2023-10-03 23:07:45 +0300

    new text and translation

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,8 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
+
 export function ErrorPage() {
+	const { t } = useTranslation();
 
 	return (
 		<section className="error-page__container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-			<p style={{ fontSize: "50px" }}>PAGE NOT FOUND</p>
+			<p style={{ fontSize: "50px" }}>{t("errorPage")}</p>
 		</section>
 	)
 }
